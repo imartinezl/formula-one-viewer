a58cbdae80771a0d828b0850e116aafd9f4035db
imartinezl <inigomlap@gmail.com>
2019-11-01 13:40:45 +0100

    Constructors colors to config

--- src/js/config/appConfig.js
+++ src/js/config/appConfig.js
@@ -2,4 +2,38 @@
 
 module.exports = {
 
+    driverColor: {
+        albon: "#1E41FF",
+        bottas: "#00D2BE",
+        gasly: "#469BFF",
+        giovinazzi: "#9B0000",
+        grosjean: "#F0D787",
+        hamilton: "#00D2BE",
+        hulkenberg: "#FFF500",
+        kevin_magnussen: "#F0D787",
+        kubica: "#FFFFFF",
+        kvyat: "#469BFF",
+        leclerc: "#DC0000",
+        max_verstappen: "#1E41FF",
+        norris: "#FF8700",
+        perez: "#F596C8",
+        raikkonen: "#9B0000",
+        ricciardo: "#FFF500",
+        russell: "#FFFFFF",
+        sainz: "#FF8700",
+        stroll: "#F596C8",
+        vettel: "#DC0000"
+    },
+    constructorColor:{
+        mercedes: "#00D2BE",
+        red_bull: "#1E41FF",
+        ferrari: "#DC0000",
+        haas: "#F0D787",
+        renault: "#FFF500",
+        alfa: "#9B0000",
+        racing_point: "#F596C8",
+        toro_rosso: "#469BFF",
+        mclaren: "#FF8700",
+        williams: "#FFFFFF"        
+    }
 }
