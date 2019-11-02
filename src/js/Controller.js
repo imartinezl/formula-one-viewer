3a4dceba7c6c9fb0e9a355d784c10a58b41d41a2
imartinezl <inigomlap@gmail.com>
2019-11-02 11:16:57 +0100

    Season select option

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -3,6 +3,7 @@
 import ErgastAPI from './components/ErgastAPI';
 import Standings from './components/Standings';
 import ScoresChart from './components/ScoresChart';
+import config from './config/appConfig';
 
 export default class FController {
 
@@ -10,20 +11,40 @@ export default class FController {
         this._ergastAPI = new ErgastAPI();
         this._standings = new Standings();
         this._scoresChart = new ScoresChart();
+
+        this._select_year = document.getElementById('select-year');
     }
 
     init() {
-        this._ergastAPI.init();
+        this.initSelectOptions();
         this._scoresChart.init();
-        this._ergastAPI.requestData().then((ergastData) => {
-            console.log(ergastData);
+        let season = this._select_year.value;
+        this.requestData(season);
+        this.initEvents();
+    }
+
+    initEvents(){
+        this._select_year.addEventListener("change", () => {
+            let season = this._select_year.value;
+            this.requestData(season);
+        });
+    }
+
+    initSelectOptions(){
+        for (let i = 0; i < config.seasons.length; i++) {
+            let option = document.createElement('option');
+            option.value = config.seasons[i];
+            option.text = config.seasons[i];
+            this._select_year.add( option ) 
+        }
+        this.season = this._select_year.value;
+    }
+    
+    requestData(season) {
+        this._ergastAPI.requestData(season).then((ergastData) => {
             this._standings.init(ergastData, this._scoresChart._scores, this._scoresChart._fastlap);
             this._scoresChart.setStandings(this._standings);
         });
-
-    }
-
-    initEvents() {
 
     }
 
--- src/js/components/ErgastAPI.js
+++ src/js/components/ErgastAPI.js
@@ -1,22 +1,20 @@
 "use strict";
-import config from '../config/appConfig';
 import HTTPHelper from '../helpers/HTTPHelper';
 
 export default class ErgastAPI {
 
     constructor() {
-        console.log('test created');
-    }
 
+    }
+    
     init() {
-        console.log('test initialized');
-        this.url = this.createURL();
+        
     }
 
-    createURL(){
+    createURL(season){
         this.baseurl = 'http://ergast.com/api';
         let series = 'f1';
-        let season = '2019';
+        //let season = '2019';
         let round = '';
         let service = 'results';
         let format = '.json';
@@ -26,7 +24,8 @@ export default class ErgastAPI {
     }
 
     
-    requestData(){
+    requestData(season){
+        this.url = this.createURL(season);
         return HTTPHelper.get(this.url);
     }
     
