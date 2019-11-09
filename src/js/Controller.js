524d86fa3673b69b3ebcac26a34ab680a9489593
imartinezl <inigomlap@gmail.com>
2019-11-09 19:37:35 +0100

    Progress Bar style and transition

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -13,35 +13,51 @@ export default class FController {
         this._scoresChart = new ScoresChart();
 
         this._select_year = document.getElementById('select-year');
+        this._progress_bar = document.getElementById('progress-bar');
+        this._progress_bar.style.display = 'none';
+
     }
 
     init() {
         this.initSelectOptions();
         this._scoresChart.init();
+        this.initEvents();
+        
         let season = this._select_year.value;
         this.requestData(season);
-        this.initEvents();
     }
 
-    initEvents(){
+    initEvents() {
         this._select_year.addEventListener("change", () => {
             let season = this._select_year.value;
             this.requestData(season);
         });
     }
 
-    initSelectOptions(){
+    initSelectOptions() {
         for (let i = 0; i < config.seasons.length; i++) {
             let option = document.createElement('option');
             option.value = config.seasons[i];
             option.text = config.seasons[i];
-            this._select_year.add( option ) 
+            this._select_year.add(option)
         }
         this.season = this._select_year.value;
     }
-    
+
+    toggleProgressBar(){
+        let d = this._progress_bar.style.display;
+        if(d === 'none'){
+            this._progress_bar.style.display = 'block';
+        }else{
+            this._progress_bar.style.display = 'none';
+        }
+    }
+
     requestData(season) {
+        this.toggleProgressBar();
+        
         this._ergastAPI.requestData(season).then((ergastData) => {
+            this.toggleProgressBar();
             console.log(ergastData);
             this._standings.init(ergastData, this._scoresChart._scores, this._scoresChart._fastlap);
             this._scoresChart.setStandings(this._standings);
